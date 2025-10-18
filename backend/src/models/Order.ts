b9ab40c53ce03e379192733bb080c01957360b3b
SajadMohtaram1378 <151362398+SajadMohtaram1378@users.noreply.github.com>
2025-10-18 18:17:27 +0330

    create model historyOrder

--- backend/src/models/Order.ts
+++ backend/src/models/Order.ts
@@ -1,6 +1,22 @@
-import mongoose from "mongoose"
+import { timeStamp } from "console";
+import mongoose, { model, Schema, Types } from "mongoose";
 
 export interface IOrder extends Document {
-    
-
+  userId: Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+  boughtCart?: string[];
 }
+
+const orderHistory = new Schema<IOrder>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+boughtCart: { type: [Object], default: [] }  },
+  { timestamps: true }
+);
+
+export const OrderHistory =
+  mongoose.models.OrderHistory || model<IOrder>("OrderHistory", orderHistory);
